cf7357db2fe229513753169e2705dd032b298a24
Mluka <921877607@qq.com>
2017-11-09 20:39:07 +0800

    add show post

--- app/main/views.py
+++ app/main/views.py
@@ -1,14 +1,22 @@
 from flask import render_template, redirect, url_for, abort, flash
 from flask_login import login_required, current_user
 from . import main
-from .forms import EditProfileForm, EditProfileAdminForm
+from .forms import EditProfileForm, EditProfileAdminForm, PostForm
 from .. import db
-from ..models import Role, User
+from ..models import Role, User, Permission, Post
 from ..decorators import admin_required
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.jinja2')
+    form = PostForm()
+    if current_user.can(Permission.WRITE_ARTICLES) and \
+            form.validate_on_submit():
+        post = Post(body=form.body.data,
+                    author=current_user._get_current_object())
+        db.session.add(post)
+        return redirect(url_for('.index'))
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.jinja2', form=form, posts=posts)
 
 @main.route('/user/<username>')
 def user(username):
